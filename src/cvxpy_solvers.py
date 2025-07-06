Add some updates and fix weighting

src/cvxpy_solvers.py
@@ -10,9 +10,7 @@ def solve_min_cost_concurrent_flow(
     traffic_lapl = np.diag(traffic_mat.sum(axis=1)) - traffic_mat
     incidence_mat = nx.incidence_matrix(graph, oriented=True).todense()
 
-    capacities = np.array(
-        list(nx.get_edge_attributes(graph, "capacities").values()), dtype=np.float32
-    )
+    capacities = np.array(list(nx.get_edge_attributes(graph, "capacities").values()), dtype=np.float32)
     costs = np.array(
         list(nx.get_edge_attributes(graph, "free_flow_times").values()),
         dtype=np.float32,
@@ -32,6 +30,7 @@ def solve_min_cost_concurrent_flow(
     costs, potentials, nonneg_duals = [cons.dual_value for cons in prob.constraints]
     return flows_ei, costs, potentials, nonneg_duals
 
+
 def solve_entropy_model_cp(
     departures: np.ndarray, arrivals: np.ndarray, distance_mat: np.ndarray, gamma: float
 ) -> np.ndarray:
@@ -47,15 +46,14 @@ def solve_entropy_model_cp(
 
     return d_ij.value
 
+
 def solve_beckmann_model_cp(traffic_mat: np.ndarray, graph: nx.DiGraph, **solver_kwargs) -> tuple:
     # TODO: test on networks where can_pass_through_zones=False
 
     traffic_lapl = np.diag(traffic_mat.sum(axis=1)) - traffic_mat
     incidence_mat = nx.incidence_matrix(graph, oriented=True).todense()
 
-    capacities = np.array(
-        list(nx.get_edge_attributes(graph, "capacities").values()), dtype=np.float32
-    )
+    capacities = np.array(list(nx.get_edge_attributes(graph, "capacities").values()), dtype=np.float32)
     ffts = np.array(
         list(nx.get_edge_attributes(graph, "free_flow_times").values()),
         dtype=np.float32,
@@ -72,8 +70,7 @@ def solve_beckmann_model_cp(traffic_mat: np.ndarray, graph: nx.DiGraph, **solver
         ffts[e]
         * (
             flows_e[e]
-            + (rhos[e] / (1 + 1 / mus[e]))
-            * (cp.pos(flows_e[e]) ** (1 + 1 / mus[e]) / capacities[e] ** (1 / mus[e]))
+            + (rhos[e] / (1 + 1 / mus[e])) * (cp.pos(flows_e[e]) ** (1 + 1 / mus[e]) / capacities[e] ** (1 / mus[e]))
         )
         for e in range(len(graph.edges))
     ]
@@ -94,18 +91,12 @@ def solve_beckmann_model_cp(traffic_mat: np.ndarray, graph: nx.DiGraph, **solver
 
 # TODO: combine in single method (??) to reuse compiled problem by using parameters
 def admm_argmin_flows(
-    traffic_mat: np.ndarray,
-    y: np.ndarray,
-    rho: float,
-    graph: nx.DiGraph,
-    **solver_kwargs
+    traffic_mat: np.ndarray, y: np.ndarray, rho: float, graph: nx.DiGraph, **solver_kwargs
 ) -> np.ndarray:
     traffic_lapl = np.diag(traffic_mat.sum(axis=1)) - traffic_mat
     incidence_mat = nx.incidence_matrix(graph, oriented=True).todense()
 
-    capacities = np.array(
-        list(nx.get_edge_attributes(graph, "capacities").values()), dtype=np.float32
-    )
+    capacities = np.array(list(nx.get_edge_attributes(graph, "capacities").values()), dtype=np.float32)
     ffts = np.array(
         list(nx.get_edge_attributes(graph, "free_flow_times").values()),
         dtype=np.float32,
@@ -120,9 +111,8 @@ def admm_argmin_flows(
     sigmas = [
         ffts[e]
         * (
-                flows_e[e]
-                + (rhos[e] / (1 + 1 / mus[e]))
-                * (cp.pos(flows_e[e]) ** (1 + 1 / mus[e]) / capacities[e] ** (1 / mus[e]))
+            flows_e[e]
+            + (rhos[e] / (1 + 1 / mus[e])) * (cp.pos(flows_e[e]) ** (1 + 1 / mus[e]) / capacities[e] ** (1 / mus[e]))
         )
         for e in range(len(graph.edges))
     ]
@@ -151,7 +141,7 @@ def admm_argmin_traffic(
     departures: np.ndarray,
     arrivals: np.ndarray,
     gamma: float,
-    **solver_kwargs
+    **solver_kwargs,
 ):
     traffic_mat = cp.Variable(shape=(len(departures), len(arrivals)), nonneg=True)
 
@@ -160,7 +150,7 @@ def admm_argmin_traffic(
     Bd_plus_Af = (incidence_mat @ flows_ei).T + traffic_lapl
 
     objective = cp.Minimize(
-        - cp.sum(cp.entr(traffic_mat)) / gamma
+        -cp.sum(cp.entr(traffic_mat)) / gamma
         # + cp.sum(cp.multiply(y, Bd_plus_Af))
         + cp.sum(cp.multiply(y, traffic_lapl))
         + rho * cp.sum_squares(Bd_plus_Af) / 2
@@ -175,16 +165,12 @@ def admm_argmin_traffic(
     return traffic_mat.value, prob.value
 
 
-def get_max_traffic_mat_mul(
-    graph: nx.Graph, traffic_mat: np.ndarray, **solver_kwargs
-) -> Optional[float]:
+def get_max_traffic_mat_mul(graph: nx.Graph, traffic_mat: np.ndarray, **solver_kwargs) -> Optional[float]:
     graph = nx.DiGraph(graph)
     traffic_lapl = np.diag(traffic_mat.sum(axis=1)) - traffic_mat
     incidence_mat = nx.incidence_matrix(graph, oriented=True).todense()
 
-    capacities = np.array(
-        list(nx.get_edge_attributes(graph, "capacities").values()), dtype=np.float32
-    )
+    capacities = np.array(list(nx.get_edge_attributes(graph, "capacities").values()), dtype=np.float32)
 
     flow = cp.Variable((len(graph.edges), traffic_mat.shape[0]))
     gamma = cp.Variable()

src/shortest_paths_gt.py
@@ -37,9 +37,7 @@ def distance_mat_gt(
 ):
     distance_mat = np.zeros((sources.size, targets.size))
     for i, source in enumerate(sources):  # i = index of source in traffic_mat
-        dist_map = shortest_distance(
-            graph, source=source, target=targets, weights=weights, pred_map=False
-        )
+        dist_map = shortest_distance(graph, source=source, target=targets, weights=weights, pred_map=False)
         distance_mat[i] = dist_map
 
     return distance_mat
@@ -51,7 +49,7 @@ def flows_on_shortest_gt(
     weights: gt.EdgePropertyMap,
     return_distance_mat: bool = False,
     sources_indexes: int = None,
-    return_flows_by_sources: bool = False , 
+    return_flows_by_sources: bool = False,
 ) -> Union[tuple[np.ndarray, np.ndarray], np.ndarray]:
     """Returns flows on edges for each ij-pair
     (obtained from flows on shortest paths w.r.t given weights(costs))
@@ -62,9 +60,7 @@ def flows_on_shortest_gt(
     traffic_mat, sources, targets = corrs.traffic_mat, corrs.sources, corrs.targets
 
     edges_arr = graph.get_edges()
-    edge_to_ind = numba.typed.Dict.empty(
-        key_type=types.UniTuple(types.int64, 2), value_type=numba.core.types.int64
-    )
+    edge_to_ind = numba.typed.Dict.empty(key_type=types.UniTuple(types.int64, 2), value_type=numba.core.types.int64)
     for i, edge in enumerate(edges_arr):
         edge_to_ind[tuple(edge)] = i
 
@@ -76,14 +72,10 @@ def flows_on_shortest_gt(
     if return_flows_by_sources:
         flows_by_sources = dict()
 
-
-
     for i, source in enumerate(sources):  # i = index of source in traffic_mat
         if sources_indexes is not None and i not in sources_indexes:
-            continue        
-        dist_map, pred_map = shortest_distance(
-            graph, source=source, target=targets, weights=weights, pred_map=True
-        )
+            continue
+        dist_map, pred_map = shortest_distance(graph, source=source, target=targets, weights=weights, pred_map=True)
         value = sum_flows_from_tree(
             source=source,
             targets=targets,
@@ -100,13 +92,9 @@ def flows_on_shortest_gt(
             distance_mat[i] = dist_map
 
     if return_flows_by_sources:
-        return flows_on_shortest_e , flows_by_sources
+        return flows_on_shortest_e, flows_by_sources
 
-    return (
-        (flows_on_shortest_e, distance_mat)
-        if return_distance_mat
-        else flows_on_shortest_e
-    )
+    return (flows_on_shortest_e, distance_mat) if return_distance_mat else flows_on_shortest_e
 
 
 def get_graphtool_graph(nx_graph: nx.Graph) -> gt.Graph:
@@ -132,13 +120,9 @@ def get_graphtool_graph(nx_graph: nx.Graph) -> gt.Graph:
     nx_nodes = list(nx_graph.nodes())
     edge_list = []
     for i, e in enumerate(nx_graph.edges()):
-        edge_list.append(
-            (*[nx_nodes.index(v) for v in e], *[attr[i] for attr in edge_attributes])
-        )
+        edge_list.append((*[nx_nodes.index(v) for v in e], *[attr[i] for attr in edge_attributes]))
 
-    gt_graph = gt.Graph(
-        edge_list, eprops=[(attr_name, "double") for attr_name in edge_attribute_names]
-    )
+    gt_graph = gt.Graph(edge_list, eprops=[(attr_name, "double") for attr_name in edge_attribute_names])
 
     return gt_graph
 

src/sinkhorn.py
@@ -9,9 +9,7 @@ np.set_printoptions(suppress=True)
 eps = 1e-6
 
 
-def d_ij(
-    lambda_l_i: np.ndarray, lambda_w_j: np.ndarray, gammaT_ij: np.ndarray
-) -> np.ndarray:
+def d_ij(lambda_l_i: np.ndarray, lambda_w_j: np.ndarray, gammaT_ij: np.ndarray) -> np.ndarray:
     return np.exp(-(1 + gammaT_ij + lambda_w_j + lambda_l_i[:, np.newaxis]))
 
 
@@ -102,13 +100,9 @@ class Sinkhorn:
     ) -> Tuple[np.ndarray, np.ndarray]:
         if k % 2 == 0:
             # print(lambda_w_tj.shape, gammaT_tij.shape, self.L_ti.shape)
-            lambda_l_i = numba_logsumexp_stable(
-                -lambda_w_j[np.newaxis, :] - 1 - gammaT_ij
-            ) - np.log(self.L_i)
+            lambda_l_i = numba_logsumexp_stable(-lambda_w_j[np.newaxis, :] - 1 - gammaT_ij) - np.log(self.L_i)
         else:
-            lambda_w_j = numba_logsumexp_stable(
-                (-lambda_l_i[:, np.newaxis] - 1 - gammaT_ij).T
-            ) - np.log(self.W_j)
+            lambda_w_j = numba_logsumexp_stable((-lambda_l_i[:, np.newaxis] - 1 - gammaT_ij).T) - np.log(self.W_j)
 
         return lambda_w_j, lambda_l_i
 
@@ -159,9 +153,7 @@ class Sinkhorn:
                 if self._criteria(lambda_l_i, lambda_w_j, gammaT_ij):
                     break
 
-            lambda_w_j, lambda_l_i = self._sinkhorn_iteration(
-                k, gammaT_ij, lambda_w_j, lambda_l_i
-            )
+            lambda_w_j, lambda_l_i = self._sinkhorn_iteration(k, gammaT_ij, lambda_w_j, lambda_l_i)
 
             k += 1
             if k == self.max_iter:
@@ -169,19 +161,13 @@ class Sinkhorn:
 
         return d_ij(lambda_l_i, lambda_w_j, gammaT_ij), lambda_l_i, lambda_w_j
 
-    def _criteria(
-        self, lambda_l_i: np.ndarray, lambda_w_j: np.ndarray, gammaT_ij: np.ndarray
-    ) -> bool:
+    def _criteria(self, lambda_l_i: np.ndarray, lambda_w_j: np.ndarray, gammaT_ij: np.ndarray) -> bool:
         traffic_mat = d_ij(lambda_l_i, lambda_w_j, gammaT_ij)
         grad_l = traffic_mat.sum(axis=1) - self.L_i
         grad_w = traffic_mat.sum(axis=0) - self.W_j
         dual_grad = np.hstack((grad_l, grad_w))
 
-        dual_grad_norm = np.linalg.norm(
-            dual_grad
-        )  # equal to constraints violation norm
-        inner_prod = (
-            -np.hstack((lambda_l_i, lambda_w_j)) @ dual_grad
-        )  # upper bound for f(x_k) - f(x^*)
+        dual_grad_norm = np.linalg.norm(dual_grad)  # equal to constraints violation norm
+        inner_prod = -np.hstack((lambda_l_i, lambda_w_j)) @ dual_grad  # upper bound for f(x_k) - f(x^*)
 
         return dual_grad_norm < self.eps and inner_prod < self.eps

src/test.py
@@ -1,4 +1,3 @@
-
 import networkx as nx
 import numpy as np
 import cvxpy as cp
@@ -15,7 +14,7 @@ from src.algs import subgd, ustm, frank_wolfe, cyclic
 from src.cvxpy_solvers import get_max_traffic_mat_mul
 from src.commons import Correspondences
 import matplotlib.pyplot as plt
-    
+
 import pandas as pd
 import time
 from matplotlib import rc
@@ -25,9 +24,8 @@ import pickle
 import datetime
 
 
-
 # по названию города ==> модель и таблицу графа
-def init_city(networks_path ,folder ,net_name ,traffic_mat_name ) :
+def init_city(networks_path, folder, net_name, traffic_mat_name):
     ##LOAD DATA
     net_file = networks_path / folder / f"{net_name}.tntp"
     traffic_mat_file = networks_path / folder / f"{traffic_mat_name}.tntp"
@@ -36,7 +34,6 @@ def init_city(networks_path ,folder ,net_name ,traffic_mat_name ) :
     n = graph.number_of_nodes()
     print(f"{graph.number_of_edges()=}, {graph.number_of_nodes()=}")
 
-
     ## BECKMANN MODEL
     beckmann_model = BeckmannModel(graph, correspondences)
     eps = 1e-6
@@ -44,97 +41,136 @@ def init_city(networks_path ,folder ,net_name ,traffic_mat_name ) :
     mean_cost = beckmann_model.graph.ep.free_flow_times.a.mean()
     # cost suboptimality <= eps * (average link cost * avg bandwidth * |E| \approx total cost when beta=1)
     eps_abs = eps * mean_cost * mean_bw * graph.number_of_edges()
-    eps_cons_abs = eps * mean_bw 
+    eps_cons_abs = eps * mean_bw
     # sum of capacity violation <= eps * average link capacity
     print(eps_abs, eps_cons_abs)
 
     result = {
-        'eps_abs' : eps_abs ,                 
+        "eps_abs": eps_abs,
     }
-    return beckmann_model , result
+    return beckmann_model, result
 
 
 # Запуск моделей с выбором числа итераций и города и аргументов метода и кастомный нейминг
-def run_method(method , name , solver_kwargs , model  ,city_name = '' , max_iter = 100) :
+def run_method(method, name, solver_kwargs, model, city_name="", max_iter=100):
     times, flows, logs, optimal = method(model, **solver_kwargs)
     dgap, time_log, other = logs
     # print(np.sum(flows))
-    experiment_attrs = ({'duality_gap' : np.abs(dgap) , 'time_log' : time_log , 'primal' : other['primal'] , 'relative_gap' : np.abs(other['relative_gap'])}  ,  name , max_iter , city_name )
-    return experiment_attrs ,other 
-def run_experiment(list_methods , model, city_name , max_iter) :
+    experiment_attrs = (
+        {
+            "duality_gap": np.abs(dgap),
+            "time_log": time_log,
+            "primal": other["primal"],
+            "relative_gap": np.abs(other["relative_gap"]),
+        },
+        name,
+        max_iter,
+        city_name,
+    )
+    return experiment_attrs, other
+
+
+def run_experiment(list_methods, model, city_name, max_iter):
     experiments = []
-    for method , name , solver_kwargs in list_methods :
-        result = run_method(method , name , solver_kwargs , model ,city_name = city_name , max_iter = max_iter)
+    for method, name, solver_kwargs in list_methods:
+        result = run_method(method, name, solver_kwargs, model, city_name=city_name, max_iter=max_iter)
         experiments.append(result[0])
     return experiments
 
+
 # список графиков ==>> вывод и сохранение
-def plot( experiments , name_output_values , time_iters = False, loglog = False , save = False  , last_quantile = 1) :
-    color_generator = plt.cm.get_cmap('tab20', len(experiments))
+def plot(experiments, name_output_values, time_iters=False, loglog=False, save=False, last_quantile=1):
+    color_generator = plt.cm.get_cmap("tab20", len(experiments))
     colors = [color_generator(i) for i in np.linspace(0, 1, len(experiments))]
     time = datetime.datetime.now().time().strftime("%H:%M")
     date = datetime.datetime.now().date()
-    experiments_folder = './experiments_results/'
-    plt.figure(figsize = (12, 6))
-    for col_id , experiment in enumerate(experiments) :
-        result , name , max_iter , city_name = experiment
-        for e,name_values in enumerate(name_output_values) :
+    experiments_folder = "./experiments_results/"
+    plt.figure(figsize=(12, 6))
+    for col_id, experiment in enumerate(experiments):
+        result, name, max_iter, city_name = experiment
+        for e, name_values in enumerate(name_output_values):
             values = result[name_values]
-            if time_iters :
-                iters = np.array(result['time_log']) - result['time_log'][0]
+            if time_iters:
+                iters = np.array(result["time_log"]) - result["time_log"][0]
                 # print(iters)
                 if len(iters) != len(values):
-                    iters = np.linspace(iters[0] , iters[-1] , len(values))
-            else :
+                    iters = np.linspace(iters[0], iters[-1], len(values))
+            else:
                 iters = np.arange(len(values))
-            plt.subplot(1, len(name_output_values), e+1)
-            
-            if name_values == 'primal' :
-                iters = iters[-int(len(iters)/last_quantile):]
-                values = values[-int(len(values)/last_quantile):]
+            plt.subplot(1, len(name_output_values), e + 1)
 
+            if name_values == "primal":
+                iters = iters[-int(len(iters) / last_quantile) :]
+                values = values[-int(len(values) / last_quantile) :]
 
-
-            if loglog == False :
-                plt.plot(iters, values , color =colors[col_id] ,label = name)
-                plt.yscale('log')
-            else :
-                plt.loglog(iters, values , color =colors[col_id] ,label = name)
-            plt.xscale('log')
-            plt.ylabel(name_values, fontsize = 12)
-            plt.xlabel('seconds' if time_iters else 'iterations' , fontsize = 12)
-            plt.title('Сходимость ' + name_values +' на городе ' + city_name + '  (max_iter = '+ str(max_iter) + ')'  )
+            if loglog == False:
+                plt.plot(iters, values, color=colors[col_id], label=name)
+                plt.yscale("log")
+            else:
+                plt.loglog(iters, values, color=colors[col_id], label=name)
+            plt.xscale("log")
+            plt.ylabel(name_values, fontsize=12)
+            plt.xlabel("seconds" if time_iters else "iterations", fontsize=12)
+            plt.title("Сходимость " + name_values + " на городе " + city_name + "  (max_iter = " + str(max_iter) + ")")
             plt.legend()
-            
-            experiment_path = experiments_folder +'iterations/'+ name_values+'_'+ name + '_' + city_name + '_' + str(max_iter) + 'iters_datetime_' + str(date)+'_' + str(time)+ '.csv'
-            if save :
-                df_values = pd.DataFrame([values,iters])
-                df_values.to_csv( experiment_path  , index=False)
-    
-    if save :
-        plt.savefig(experiments_folder +'pictures/'+ 'Experiment_' + ' '.join(name_output_values)+ '_date_'+ str(date) +'_time_'+str(time)+ '_city_' + city_name + '_' + str(max_iter) + 'iters' + '.png')
+
+            experiment_path = (
+                experiments_folder
+                + "iterations/"
+                + name_values
+                + "_"
+                + name
+                + "_"
+                + city_name
+                + "_"
+                + str(max_iter)
+                + "iters_datetime_"
+                + str(date)
+                + "_"
+                + str(time)
+                + ".csv"
+            )
+            if save:
+                df_values = pd.DataFrame([values, iters])
+                df_values.to_csv(experiment_path, index=False)
+
+    if save:
+        plt.savefig(
+            experiments_folder
+            + "pictures/"
+            + "Experiment_"
+            + " ".join(name_output_values)
+            + "_date_"
+            + str(date)
+            + "_time_"
+            + str(time)
+            + "_city_"
+            + city_name
+            + "_"
+            + str(max_iter)
+            + "iters"
+            + ".png"
+        )
     plt.tight_layout()
     plt.show()
-    
+
 
 # Кастомно прочекать графики по значениям целевой функции в file.csv
-def display(filename , last_iters = 0 ) :
-    values = pd.read_csv(filename ).values
-    
-    plt.figure(figsize = (10, 5))
+def display(filename, last_iters=0):
+    values = pd.read_csv(filename).values
+
+    plt.figure(figsize=(10, 5))
 
     iters = np.arange(len(values) if last_iters == 0 else last_iters)
     n = len(iters)
-    values = values[-n:] 
-    plt.plot(iters, values  ,label = filename)
-    plt.ylabel('values', fontsize = 12)
-    plt.xlabel('iterations', fontsize = 12)
-    plt.title('Сходимость values ')
+    values = values[-n:]
+    plt.plot(iters, values, label=filename)
+    plt.ylabel("values", fontsize=12)
+    plt.xlabel("iterations", fontsize=12)
+    plt.title("Сходимость values ")
     plt.legend()
-    plt.yscale('log')
+    plt.yscale("log")
     plt.show()
 
 
-
 # display('./experiments_results/Frank Wolfe_SiouxFalls_100iters.csv' , 10)
-

src/utils.py
@@ -1,41 +1,42 @@
-
 import numba
 from numba.core import types
 import numpy as np
 
 
-def check_correct_flow(flow, model ):    
+def check_correct_flow(flow, model):
     num_nodes, num_edges = model.graph.num_vertices(), model.graph.num_edges()
-    traffic_mat, sources, targets = model.correspondences.traffic_mat, model.correspondences.sources, model.correspondences.targets
-    edges_arr = model.graph.get_edges()
-    edge_to_ind = numba.typed.Dict.empty(
-        key_type=types.UniTuple(types.int64, 2), value_type=numba.core.types.int64
+    traffic_mat, sources, targets = (
+        model.correspondences.traffic_mat,
+        model.correspondences.sources,
+        model.correspondences.targets,
     )
+    edges_arr = model.graph.get_edges()
+    edge_to_ind = numba.typed.Dict.empty(key_type=types.UniTuple(types.int64, 2), value_type=numba.core.types.int64)
     for i, edge in enumerate(edges_arr):
         edge_to_ind[tuple(edge)] = i
 
     node_traffic = model.correspondences.node_traffic_mat
-    edges = [ e for e in model.graph.edges()]    
-    vertices = [ e for e in model.graph.vertices()]
-    
-    corrects = [] 
+    edges = [e for e in model.graph.edges()]
+    vertices = [e for e in model.graph.vertices()]
+
+    corrects = []
     for j in range(len(vertices)):
         value = 0
-        traffic_from_j = sum(traffic_mat[j]) - sum(traffic_mat[:,j])
+        traffic_from_j = sum(traffic_mat[j]) - sum(traffic_mat[:, j])
         v = vertices[j]
-        
+
         for e_out in v.out_edges():
-            e_out_ind = edge_to_ind[(int(e_out.source()) ,int(e_out.target()))]
+            e_out_ind = edge_to_ind[(int(e_out.source()), int(e_out.target()))]
             value += flow[e_out_ind]
         for e_in in v.in_edges():
-            e_in_ind = edge_to_ind[(int(e_in.source()) ,int(e_in.target()))]
+            e_in_ind = edge_to_ind[(int(e_in.source()), int(e_in.target()))]
             value -= flow[e_in_ind]
-        corrects.append((j , traffic_from_j - value))
+        corrects.append((j, traffic_from_j - value))
     corrects = np.array(corrects)
-    return np.all(np.abs(corrects[:,1]) < 1e-8 ) , corrects
+    return np.all(np.abs(corrects[:, 1]) < 1e-8), corrects
 
 
-def full_flows_from_dict( flows_dict ):
+def full_flows_from_dict(flows_dict):
     assert len(flows_dict.keys()) != 0
     flows = None
     for key in flows_dict.keys():
@@ -45,7 +46,8 @@ def full_flows_from_dict( flows_dict ):
             flows += flows_dict[key]
     return flows
 
-def sum_flow_dicts( flow_dicts , weights = None):
+
+def sum_flow_dicts(flow_dicts, weights=None):
     new_flow_dict = dict()
     for i, flow_dict in enumerate(flow_dicts):
         if weights is not None:
@@ -54,14 +56,15 @@ def sum_flow_dicts( flow_dicts , weights = None):
             weight = 1
 
         for key in flow_dict.keys():
-            
+
             if key not in new_flow_dict.keys():
                 new_flow_dict[key] = weight * flow_dict[key]
             else:
                 new_flow_dict[key] += weight * flow_dict[key]
     return new_flow_dict
 
-def sum_flow_dicts_without_intersection( flow_dicts , weights = None ):
+
+def sum_flow_dicts_without_intersection(flow_dicts, weights=None):
     new_flow_dict = dict()
     current_keys = []
     # print('test full sum', weights)
@@ -73,14 +76,15 @@ def sum_flow_dicts_without_intersection( flow_dicts , weights = None ):
         # print('Add')
         for key in flow_dict.keys():
             if key not in current_keys:
-                
+
                 # print(key , weight , flow_dict[key])
                 new_flow_dict[key] = weight * flow_dict[key]
                 current_keys.append(key)
         # print('keys curr:' , current_keys)
     return new_flow_dict
 
-def sum_flow_dicts_with_intersection( flow_dicts , weights = None):
+
+def sum_flow_dicts_with_intersection(flow_dicts, weights=None):
     new_flow_dict = dict()
     key_sum_weights = dict()
     # print('TEST SUMM ', weights)
@@ -98,17 +102,17 @@ def sum_flow_dicts_with_intersection( flow_dicts , weights = None):
                     new_flow_dict[key] = weight * flow_dict[key]
                 else:
                     new_flow_dict[key] += weight * flow_dict[key]
-            
+
                 if key not in key_sum_weights.keys():
-                    key_sum_weights[key] = weight 
+                    key_sum_weights[key] = weight
                 else:
                     key_sum_weights[key] += weight
 
     # print('keySUMweight:' , key_sum_weights)
     for key in new_flow_dict.keys():
-        if key_sum_weights[key] != 0 :
+        if key_sum_weights[key] != 0:
             new_flow_dict[key] /= key_sum_weights[key]
-    
+
     # print('result dict',new_flow_dict)
 
     return new_flow_dict